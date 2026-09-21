f0ae621681b5783fcfba37a0af7fd9c3b5e0bebc
Joris Snoek <joris@lucius.digital>
2026-09-21 10:15:44 +0200

    Update setup-private-files.php

--- scripts/composer/setup-private-files.php
+++ scripts/composer/setup-private-files.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * @file
  * Creates private files folder and setting.
  *
  * This is needed here, because in profile .install this generates errors.
@@ -26,9 +27,6 @@ if (!is_dir($privatePath)) {
         sprintf('Unable to create private directory: %s', $privatePath)
     );
   }
-  else {
-    print "Private directory created (/private).\n";
-  }
 }
 
 // Create settings.php from default.settings.php if it doesn't exist.
@@ -41,32 +39,25 @@ if (!file_exists($settingsPath)) {
 
   if (!copy($defaultSettingsPath, $settingsPath)) {
     throw new RuntimeException(
-        sprintf('Unable to copy %s to %s', $defaultSettingsPath, $settingsPath)
+        sprintf(
+            'Unable to copy %s to %s',
+            $defaultSettingsPath,
+            $settingsPath
+        )
     );
   }
 }
 
-// Configure Drupal's private file path.
+// Add private file path to settings.php if it isn't already present.
 $contents = file_get_contents($settingsPath);
-
-$defaultSetting = "# \$settings['file_private_path'] = '';";
 $privateSetting = "\$settings['file_private_path'] = '../private';";
+$contents = rtrim($contents) . "\n\n" . $privateSetting . "\n";
 
-if (strpos($contents, $privateSetting) === FALSE) {
-  $contents = str_replace(
-      $defaultSetting,
-      $privateSetting,
-      $contents
+if (file_put_contents($settingsPath, $contents) === FALSE) {
+  throw new RuntimeException(
+      sprintf('Unable to update settings.php: %s', $settingsPath)
   );
-
-  if (file_put_contents($settingsPath, $contents) === FALSE) {
-    throw new RuntimeException(
-        sprintf('Unable to update settings.php: %s', $settingsPath)
-    );
-  }
-  else {
-    print "Copied default.settings.php to settings.php to configure \$settings['file_private_path'] = '../private'; .\n";
-  }
 }
 
+echo "Private files directory and settings.php configured successfully.\n";
 
